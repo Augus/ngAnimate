726c2009f488604f0f09b1dd14e4f70bf72fa106
Augus <augus790302@gmail.com>
2013-10-07 23:00:15 +0800

    first version

---
+++ js/app.js
@@ -0,0 +1,59 @@
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
+
+var app = angular.module('app', ['ngAnimate']);
+
+
+function AppController ($scope, $rootScope, $http, $timeout) {
+
+	$scope.list = [];
+	$scope.animations = ["toggle", 
+						"spinToggle", 
+						"slideLeft", 
+						"slideRight", 
+						"slideTop", 
+						"slideDown", 
+						"bouncySlideLeft", 
+						"bouncySlideRight", 
+						"bouncySlideTop", 
+						"bouncySlideDown", 
+						"scaleFade", 
+						"scaleFadeIn", 
+						"bouncyScaleIn", 
+						"flipIn", 
+						"rotateIn"];
+
+	$scope.add = function (animation) {
+		$scope.animation = animation;
+		for (var i = 0; i < 3; i++) {
+			pushDelay($scope.list, { title : "item" }, i, 100);
+		};		
+	}
+
+	$scope.remove = function (item) {
+		var index = $scope.list.indexOf(item);
+		$scope.list.remove(index);
+	}
+
+	$scope.clean = function () {
+		for (var i = 0; i < $scope.list.length; i++) {
+			removeDelay($scope.list, i, 100);
+		};
+	}
+
+	function pushDelay (array, item, i, duration) {
+		$timeout(function () {
+			array.push(item);
+		}, duration * i);
+	}
+
+	function removeDelay (array, i, duration) {
+		$timeout(function () {
+			array.pop();
+		}, duration * i);
+	}
+}
+
